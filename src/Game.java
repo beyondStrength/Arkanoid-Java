d6dcf3a95487325d2b70a0f228e239336e5432ee
beyondStrength <iuri.turano@gmail.com>
2021-04-24 12:27:56 -0300

    added keyboard handler

--- src/Game.java
+++ src/Game.java
@@ -1,29 +1,32 @@
-import java.awt.Graphics;
-import java.awt.Color;
-import java.awt.Dimension;
+import java.awt.*;
+import java.awt.event.*;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 
-public class Game extends JPanel  {
+public class Game extends JPanel{
     JFrame frame = new JFrame();
     Dimension size;
     public static double dW, dH;
     Paddle p = new Paddle();
+    Ball b = new Ball();
 
     void Start()    {
         this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.frame.setTitle("Arkanoid");
         this.frame.setSize(1280,720);
         this.frame.setVisible(true);
-        this.frame.setResizable(true);
+        this.frame.setResizable(false);
+        this.frame.add(b);
         this.frame.add(p);
+        this.frame.addKeyListener(new InputHandler());
         size = frame.getBounds().getSize();
         dH = size.getHeight();
         dW = size.getWidth();
 
         p.Start();
+        b.Start();
 
     }
 
@@ -32,6 +35,19 @@ public class Game extends JPanel  {
         dH = size.getHeight();
         dW = size.getWidth();
         p.Update();
+        b.Update();
+        p.repaint();
+    }
+
+    public class InputHandler extends KeyAdapter{
+        public void keyPressed(KeyEvent e)  {
+            p.keyPressed(e);
+
+        }
+        public void keyReleased(KeyEvent e) {
+            p.keyReleased(e);
+        }
+        
     }
 
     public static void main(String[] args) throws Exception {
--- src/Paddle.java
+++ src/Paddle.java
@@ -1,11 +1,12 @@
-import java.awt.Graphics;
-import java.awt.Graphics2D;
+import java.awt.*;
+import java.awt.event.*;
 
 import javax.swing.JPanel;
 
 public class Paddle extends JPanel {
 
     int x,y,w,h;
+    int speed;
 
 
     public void paintComponent(Graphics g){
@@ -14,6 +15,26 @@ public class Paddle extends JPanel {
 
     }
 
+    public void keyPressed(KeyEvent e){
+        if (e.getKeyCode() == KeyEvent.VK_RIGHT){
+            speed=10;
+        }
+        if (e.getKeyCode() == KeyEvent.VK_LEFT){
+            speed=-10;    
+        }
+    }
+
+    public void keyReleased(KeyEvent e){
+        if (e.getKeyCode() == KeyEvent.VK_RIGHT){
+            if (speed>0)
+                speed = 0;
+        }
+        if (e.getKeyCode() == KeyEvent.VK_LEFT){
+            if (speed<0)
+                speed = 0;    
+        }
+    }
+    
     void Start()    {
         w = (int)Game.dW/8;
         h = (int)Game.dH/16;
@@ -21,8 +42,17 @@ public class Paddle extends JPanel {
         y = ((int)Game.dH)-(h*4);
     }
 
+    void CollisionHandler() {
+        if (x+w > Game.dW)
+            x = (int)Game.dW - w;
+        if (x < 0)
+            x = 0;
+    }
+
+
     void Update()   {
-        x++;
-        this.repaint();
+        x+=speed;
+        CollisionHandler();
+        //this.repaint();
     }
 }
