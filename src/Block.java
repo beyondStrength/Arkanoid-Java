19894fdc219784572252b68f0e6737e6ae1fb016
DandySolid46 <44729275+DandySolid46@users.noreply.github.com>
2021-05-29 23:53:16 -0300

    Update Block.java

--- src/Block.java
+++ src/Block.java
@@ -3,6 +3,7 @@ import java.awt.Color;
 import java.util.Random;
 
 public class Block {
+    Ball ball;
     int x,y,w,h;
     boolean alive;
 
@@ -15,6 +16,27 @@ public class Block {
         alive = true;
     }
 
+
+    int getLeft() {
+        return this.x;
+    }
+    int getMiddleX() {
+        return this.x + (this.w/2);
+    }
+    int getRight() {
+        return this.x + this.w;
+    }
+    int getDown() {
+        return this.y + this.h;
+    }
+    int getMiddleY() {
+        return this.y + (this.h/2);
+    }
+    int getUp() {
+        return this.y;
+    }
+
+
     int getRand(int min, int max) {
         Random r = new Random();
         return r.nextInt((max - min) + 1) + min;
@@ -23,5 +45,14 @@ public class Block {
     void Draw(Graphics g) {
         g.setColor(new Color((int)(Math.random() * 0x1000000)));
         g.drawRect(x, y, w, h);
+    }
+    void Update(){
+        CollisionHandler();
+    }
+    void CollisionHandler() {
+        if (ball.getLeft() < this.getRight() &ball.getLeft() < this.getLeft()){
+
+        }
+
     }
 }
